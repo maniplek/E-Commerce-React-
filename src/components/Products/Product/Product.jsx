911023101d7462a4a70709a0edae125b76b0754f
maniplek <maniplek81@gmail.com>
2022-08-14 12:49:46 +0200

    added backed store

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -6,18 +6,20 @@ import useStyles from './style';
 
 const Product = ({product}) => {
     const classes = useStyles();
+// console.log(product)
+// return <div>test</div>
   return (
     <Card className={classes.root}>
-        <CardMedia className={classes.media} image={product.image} tittle={product.name}/>
+        <CardMedia className={classes.media} image={product.image.url} tittle={product.name}/>
         <CardContent>
             <div className={classes.CardContent}>
                 <Typography variant='h5' gutterBottom >
                     {product.name}
                 </Typography>
                 <Typography variant='h5' gutterBottom >
-                    {product.price}
+                    {product.price.formatted_with_symbol}
                 </Typography>
-                <Typography variant='body2'>{product.description}</Typography>
+                <Typography dangerouslySetInnerHTML={{ __html: product.description}} variant='body2'/>
             </div>
         </CardContent>
         <CardActions disableSpacing className={classes.cardActions}>
