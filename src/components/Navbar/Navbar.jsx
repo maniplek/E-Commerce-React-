01aa8f895724b34fa42554783cc35c88f11f0fb1
maniplek <maniplek81@gmail.com>
2022-08-14 13:02:04 +0200

    updated name

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,8 +12,8 @@ const Navbar = () => {
     <AppBar position='fixed' className={classes.AppBar}color='inherit'>
         <Toolbar>
             <Typography variant='h6' className={classes.tittle} color='inherit'>
-                <img src={logo} alt='NIHE' height='25px' className={classes.image}  />
-                MY SNEAKERS
+                <img src={logo} alt='NIHE' height='25px' className={classes.image}/>MY SNEAKERS
+                
             </Typography>
             <div className={classes.grow} />
             <div className={classes.button} >
